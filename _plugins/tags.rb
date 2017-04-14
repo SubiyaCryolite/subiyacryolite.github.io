fc887455dac017ba5dc5c6ce081b43fa3b613edd
ifunga ndana <ifungandana@gmail.com>
2017-04-14 15:26:10 +0200

    Attempting to fix issue with tags

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -1,24 +1,33 @@
-module Paperplanes
-  class TagPage < Jekyll::Page
-    def initialize(site, base, category, posts)
+module Jekyll
+  class TagIndex < Page
+    def initialize(site, base, dir, tag)
       @site = site
       @base = base
-      @dir = 'tags' 
-      @name = "#{category}.html"
-
-      process(@name)
-      read_yaml(File.join(base, '_layouts'), 'tag.html')
-      data['tag'] = category
-      data['posts'] = posts
-      data['title'] = "Tag archive: #{category}"
+      @dir = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['tag'] = tag
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
+      tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
+      self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
     end
   end
-
-  class Tags < Jekyll::Generator
+  class TagGenerator < Generator
+    safe true
     def generate(site)
-      site.tags.each do |tag, posts|
-        site.pages << TagPage.new(site, site.source, tag, posts.reverse)
+      if site.layouts.key? 'tag'
+        dir = site.config['tag_dir'] || 'tag'
+        site.tags.keys.each do |tag|
+          write_tag(site, File.join(dir, tag), tag)
+        end
       end
+    end
+    def write_tag(site, dir, tag)
+      index = TagIndex.new(site, site.source, dir, tag)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
     end
   end
 end
